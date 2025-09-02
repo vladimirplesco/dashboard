e7e92de2400c0855bfa3f990aaf25a17a030b802
Vladimir Plesco <pleshco@deeplace.md>
2025-09-02 14:24:25 +0300

    fetch data for the Card components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,6 +7,7 @@ import {
   LatestInvoiceRaw,
   Revenue,
 } from './definitions';
+
 import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
